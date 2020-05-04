49a4c6da481d436dbc7e015501abd49d7910b18d
kitekii <tim720719@163.com>
2020-05-04 23:06:51 +0800

    have an awesome test

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,25 @@
 #include <ArgHandle.h>
 #include <FTPAPI.h>
 #include <View.h>
+
+using namespace std;
 using namespace ELFTP;
 int main(int argc, char* argv[]) {
     // SOCKET so =  FTPAPI::socket_connect("120.0.0.1", 27);
+    View::getInstance()->Init();
 
-    if (argc <= 1) {  // TODO 全局模式
+    ArgHandle::getInstance()->BindCommand("help", 0, [&](vector<string> args) {
+        cout << "Yes We Made It!!!" << endl;
+    });
+    ArgHandle::getInstance()->BindCommand("exit", 0, [&](vector<string> args) {
+        cout << "Bye!" << endl;
+        ArgHandle::getInstance()->Exit();
+    });
+
+    if (argc <= 1) {  // FIXME 是否考虑全局模式？
         ArgHandle::getInstance()->ReadArgs(true);
     } else {
-        ArgHandle::getInstance()->Parse(argv + 1);
+        ArgHandle::getInstance()->Parse(argc, argv);
     }
-
-    View::getInstance()->Init();
-    View::getInstance()->PrintLayout();
     return 0;
 }
