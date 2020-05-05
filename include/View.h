b4263523ed5b3278b435a181eba514eb83391088
kitekii <tim720719@163.com>
2020-05-05 11:18:44 +0800

    define some view functions

--- include/View.h
+++ include/View.h
@@ -12,16 +12,35 @@ class View : public Singleton<View> {
 
    public:
     void Init();
-    // Essential Functions
-    void MoveCursorTo(int x, int y);
 
+    /* Essential Functions */
+    // 移动指针
+    void MoveCursorTo(int x, int y);
+    // 彩色输出
     void ColorPrintf(char* str,
                      WORD cl = FOREGROUND_BLUE | FOREGROUND_INTENSITY);
 
+    /* Draw Functions */
     // 输出一个矩阵边框
     void PrintBorder(char ch, int x, int y, int w, int h);
+    // 清空一个区域
+    void Clear(int x, int y, int w, int h);
+    // 清空全部区域
+    void ClearScreen();
+    // 带区域限制输出文本
+    void PrintTextarea(char*,
+                       int x,
+                       int y,
+                       int w = -1,
+                       int h = -1,
+                       bool wordwarp = true);
 
+    /* Application Functions */
     // 输出窗体
     void PrintLayout();
+    // 输出信息
+    void PrintLog();
+    // 输出错误信息
+    void PrintErr();
 };
 }  // namespace ELFTP
