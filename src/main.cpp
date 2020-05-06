65ba5c890dc4e665ffa13cb618280ece88ef44e8
Kitekii <tim720719@163.com>
2020-05-06 22:51:50 +0800

    最 高 指 令

--- src/main.cpp
+++ src/main.cpp
@@ -21,9 +21,28 @@ int main(int argc, char* argv[]) {
             cout << args[0] << endl;
         })
         ->BindCommand("jump", 1, [&](vector<string> args) {
+            // 连接ftp服务器
+            // TODO 处理一下登录
             char ip[20];
             strcpy(ip, args[0].c_str());
             FTPAPI::socket_connect(ip, 27);
+        })
+        ->BindCommand("cd", 1, [&](vector<string> args) {
+            // TODO CWD相关
+            // 要分析 . 和 .. 还有相对目录问题，可能要一个静态类存这些东西
+
+        })
+        ->BindCommand("ls", 0, [&](vector<string> args) {
+            // TODO LIST相关
+            // 列出内容呗
+
+        })
+        ->BindCommand("retr", 2, [&](vector<string> args) {
+            // TODO RETR文件下载
+            // args[0] - source
+            // args[1] - target:以"/"结尾意味着直接放入文件夹，其他情况就改名如何
+            // 也要处理一下目录问题
+
         });
 
     if (argc <= 1) {  // FIXME 是否考虑全局模式？
